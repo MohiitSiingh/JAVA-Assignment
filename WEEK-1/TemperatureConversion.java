9be6e40ba15e4844014205d0384012b215dfcc5c
Mohit <mohiitsiingh10@gmail.com>
2026-08-03 20:43:27 +0530

    initial

---
+++ WEEK-1/TemperatureConversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class TemperatureConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double celsius = sc.nextDouble();
+        double fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+        System.out.println("Fahrenheit: " + fahrenheit);
+        sc.close();
+    }
+}
