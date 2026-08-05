814508ef0ab2c1a568beca6afca90307c9276adf
Mohit <mohiitsiingh10@gmail.com>
2026-08-05 03:38:39 +0000

    Aug 5, 2026, 9:08 AM

--- week2/src/GradingSystem.java
+++ week2/src/GradingSystem.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class GradingSystem {
 public static void main(String[] args) {
+    System.out.println("Enter Marks: ");
+    Scanner input = new Scanner(System.in);
     
 }
 }
