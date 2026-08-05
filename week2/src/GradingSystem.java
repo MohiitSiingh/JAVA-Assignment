9fbdb47b616a218ba5972615c2db8355cd67f9cb
Mohit <mohiitsiingh10@gmail.com>
2026-08-05 03:44:15 +0000

    Aug 5, 2026, 9:14 AM

--- week2/src/GradingSystem.java
+++ week2/src/GradingSystem.java
@@ -3,6 +3,11 @@ public class GradingSystem {
 public static void main(String[] args) {
     System.out.println("Enter Marks: ");
     Scanner input = new Scanner(System.in);
+    int marks = input.nextInt();
+    System.out.println("marks between 80 - 100 = distinction");
+    System.out.println("marks between 70-79 : merit");
+    System.out.println("Marks between 50 to 69 = pass");
+    System.out.println("Marks between 0 to 49 = fail");
     
 }
 }
