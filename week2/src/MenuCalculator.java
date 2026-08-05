d35b56da103a57dc41b75331529a04101734b914
Mohit <mohiitsiingh10@gmail.com>
2026-08-05 15:04:59 +0000

    Aug 5, 2026, 8:34 PM

--- week2/src/MenuCalculator.java
+++ week2/src/MenuCalculator.java
@@ -4,7 +4,8 @@ public static void main(String[] args) {
    System.out.println("Enter choice : ");
    Scanner input = new Scanner(System.in);
    int Choice = input.nextInt();
-   
+   System.out.println("Enter number 1: ");
+   Scanner sc =  new Scanner(System.in);
 }
 
 }
