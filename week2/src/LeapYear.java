1d2c26b120108af43a03effdc8dd4b4fd94cad35
Mohit <mohiitsiingh10@gmail.com>
2026-08-05 04:18:48 +0000

    Aug 5, 2026, 9:48 AM

--- week2/src/LeapYear.java
+++ week2/src/LeapYear.java
@@ -1,4 +1,17 @@
 import java.util.Scanner;
 public class LeapYear {
-
+    public static void main(String[] args) {
+        System.out.println("Enter a year");
+        Scanner input = new Scanner(System.in);
+        int year = input.nextInt();
+boolean LeapYear = isLeapYear(year);
+System.out.println("This is leap year : "+ LeapYear);
+input.close();
+    }
+    static boolean isLeapYear(int year){
+        if(year%4==0 && year%100!= 0 || year % 400 == 0)
+            return true;
+        else 
+            return false;
+    }
 }
