f65989db46906cc744391f33f1b58e7178116db1
Mohit <mohiitsiingh10@gmail.com>
2026-08-09 03:41:32 +0000

    Aug 9, 2026, 9:11 AM

--- week2/src/PrimeNumber.java
+++ week2/src/PrimeNumber.java
@@ -1,6 +1,18 @@
 public class PrimeNumber {
 public static void main(String[] args) {
     // System.out.println("Enter")
+    boolean NumIsPrime = isPrime(23);
+ System.out.println("The number is prime: " + NumIsPrime);
+}
+static boolean isPrime (int prime){
+if(prime<2)
+    return false;
+for(int divisor =2 ; divisor * divisor <= prime; divisor++){
+if(prime % divisor == 0){
+    return false;
+        }
+    }
+return true;
+    }
 }
 
-}
