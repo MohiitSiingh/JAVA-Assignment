48cca577baa96c6dade8e88c026b47aecf571af1
Mohit <mohiitsiingh10@gmail.com>
2026-08-12 04:45:26 +0000

    Aug 12, 2026, 10:15 AM

--- week2/src/LoopPatterns.java
+++ week2/src/LoopPatterns.java
@@ -2,55 +2,56 @@ public class LoopPatterns {
     public static void main(String[] args) {
         System.out.println("Printing different pattern of number and * ");
         System.out.println("Pattern A : " );
-        RectangularPattern();
+        RectangularPattern(4);
 
         System.out.println("Pattern B : " );
-        TriangularPattern();
+        TriangularPattern(4);
 
          System.out.println("Pattern C : " );
-         TriangularPatternUp();
+         TriangularPatternUp(4);
 
           System.out.println("Pattern D : " );
-          PatternforNumTriangular();
+          PatternforNumTriangular(4);
 
           System.out.println("Pattern E : " );
-          PatternforNum();
+          PatternforNum(4);
 
     }
-    static void RectangularPattern(){
-        for(int i = 0 ; i < 4 ; i++){
-            for(int j = 0 ; j < 4 ; j++){
+    static void RectangularPattern(int size){
+       
+        for(int i = 0 ; i < size ; i++){
+            for(int j = 0 ; j < size ; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
-    static void TriangularPattern(){
-        for (int i = 0 ; i < 4 ; i++){
+    static void TriangularPattern(int size){
+        for (int i = 0 ; i < size ; i++){
             for(int j = 0 ; j <= i ; j++){
                 System.out.print("* ");
             }
              System.out.println();
         }
     }
-       static void TriangularPatternUp(){
-        for (int i = 4 ; i > 0 ; i--){
+       static void TriangularPatternUp(int size){
+        for (int i = size ; i > 0 ; i--){
             for(int j = 0 ; j < i ; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
-    static void PatternforNumTriangular(){
-        for (int i = 1 ; i <= 4 ; i++){
+    static void PatternforNumTriangular(int size){
+        for (int i = 1 ; i <= size ; i++){
             for(int j = 1 ; j <= i; j++){
                 System.out.print(" " + j);
             }
              System.out.println();
         }
     }
-     static void PatternforNum(){
-        for (int i = 1 ; i <= 4 ; i++){
+     static void PatternforNum(int size){
+        for (int i = 1 ; i <= size ; i++){
             for(int j = 1 ; j <= i; j++){
                 System.out.print(" " +i);
             }
