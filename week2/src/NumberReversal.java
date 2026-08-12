26a97dc23ce7e6cb0766621c04dc275d65019d4c
Mohit <mohiitsiingh10@gmail.com>
2026-08-12 03:46:28 +0000

    Aug 12, 2026, 9:16 AM

--- week2/src/NumberReversal.java
+++ week2/src/NumberReversal.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class NumberReversal {
 public static void main(String[] args) {
-    
+    System.out.println("Enter the number to be reversed: ");
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
 }
 }
