10b846b9500339508f975c7186b29e932908806c
Mohit <mohiitsiingh10@gmail.com>
2026-08-12 03:24:23 +0000

    Aug 12, 2026, 8:54 AM

--- week2/src/GreatestCommonDivisor.java
+++ week2/src/GreatestCommonDivisor.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class GreatestCommonDivisor {
 public static void main(String[] args) {
+    System.out.println("Enter first integer: ");
+    Scanner input = new Scanner(System.in);
     
 }
 }
