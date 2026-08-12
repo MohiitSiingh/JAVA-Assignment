b5b694568914bfb65897869b6ab9941f90ee4f63
Mohit <mohiitsiingh10@gmail.com>
2026-08-12 04:26:41 +0000

    Aug 12, 2026, 9:56 AM

--- week2/src/LoopPatterns.java
+++ week2/src/LoopPatterns.java
@@ -30,26 +30,29 @@ public class LoopPatterns {
             for(int j = 0 ; j <= i ; j++){
                 System.out.print("* ");
             }
+             System.out.println();
         }
     }
        static void TriangularPatternUp(){
         for (int i = 0 ; i < 4 ; i++){
             for(int j = i ; j > 0 ; j++){
-                System.out.println("* ");
+                System.out.print("* ");
             }
+            
         }
     }
     static void PatternforNumTriangular(){
         for (int i = 1 ; i <= 4 ; i++){
             for(int j = 1 ; j <= i; j++){
-                System.out.println();
+                System.out.print(j);
             }
+             System.out.println();
         }
     }
      static void PatternforNum(){
         for (int i = 1 ; i <= 4 ; i++){
             for(int j = 1 ; j <= i; j++){
-                System.out.println(i);
+                System.out.print(i);
             }
         }
     }
