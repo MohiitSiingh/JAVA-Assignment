dcf28443cbe802ed2fb3ab3e3680e9f796ee4eff
Mohit <mohiitsiingh10@gmail.com>
2026-08-12 03:27:28 +0000

    Aug 12, 2026, 8:57 AM

--- week2/src/GreatestCommonDivisor.java
+++ week2/src/GreatestCommonDivisor.java
@@ -3,6 +3,16 @@ public class GreatestCommonDivisor {
 public static void main(String[] args) {
     System.out.println("Enter first integer: ");
     Scanner input = new Scanner(System.in);
-    
+    int first = input.nextInt();
+    System.out.println("Enter Second integer: ");
+    int second = input.nextInt();
+
+}
+static int gcd(int first, int second){
+   int  remainder = first % second;
+    first = second;
+    second = remainder;
+    return remainder;
+
 }
 }
