4d5c3868497c6c19e964d22dcc4a711f710b2673
Mohit <mohiitsiingh10@gmail.com>
2026-08-12 04:20:03 +0000

    Aug 12, 2026, 9:50 AM

--- week2/src/LoopPatterns.java
+++ week2/src/LoopPatterns.java
@@ -1,5 +1,48 @@
 public class LoopPatterns {
+    public static void main(String[] args) {
+        System.out.println("Printing different pattern of number and * ");
+        System.out.println("Pattern A : " );
+        RectangularPattern();
+
+        System.out.println("Pattern B : " );
+        TriangularPattern();
+
+         System.out.println("Pattern C : " );
+         TriangularPatternUp();
+    }
     static void RectangularPattern(){
-        
+        for(int i = 0 ; i < 4 ; i++){
+            for(int j = 0 ; j < 4 ; j++){
+                System.out.println("* ");
+            }
+        }
+    }
+    static void TriangularPattern(){
+        for (int i = 0 ; i < 4 ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                System.out.println("* ");
+            }
+        }
+    }
+       static void TriangularPatternUp(){
+        for (int i = 0 ; i < 4 ; i++){
+            for(int j = i ; j > 0 ; j++){
+                System.out.println("* ");
+            }
+        }
+    }
+    static void PatternforNumTriangular(){
+        for (int i = 1 ; i <= 4 ; i++){
+            for(int j = 1 ; j <= i; j++){
+                System.out.println();
+            }
+        }
+    }
+     static void PatternforNum(){
+        for (int i = 1 ; i <= 4 ; i++){
+            for(int j = 1 ; j <= i; j++){
+                System.out.println(i);
+            }
+        }
     }
 }
