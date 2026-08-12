c0d09c50181a7a9a3978e5e7443a2d517958a577
Mohit <mohiitsiingh10@gmail.com>
2026-08-12 03:52:55 +0000

    Aug 12, 2026, 9:22 AM

--- week2/src/NumberReversal.java
+++ week2/src/NumberReversal.java
@@ -4,5 +4,16 @@ public static void main(String[] args) {
     System.out.println("Enter the number to be reversed: ");
     Scanner input = new Scanner(System.in);
     int num = input.nextInt();
+    int Rev = reverseNumber(num);
+    System.out.print("the number after reversing is : " + Rev);
+}
+static int reverseNumber(int number){
+    int reversed = 0;
+    while (number != 0){
+    int digit = number % 10;
+     reversed = reversed * 10 + digit;
+     number = number / 10;
+    }
+    return reversed;
 }
 }
