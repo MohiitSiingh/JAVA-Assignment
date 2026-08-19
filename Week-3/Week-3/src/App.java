9ed522c68151e11b6606e703fb3dee8433321c45
Mohit <mohiitsiingh10@gmail.com>
2026-08-19 04:59:39 +0000

    Aug 19, 2026, 10:29 AM

--- Week-3/Week-3/src/App.java
+++ Week-3/Week-3/src/App.java
@@ -19,6 +19,10 @@ public class App {
         MatrixOperations MO = new MatrixOperations();
         int [][]arr = {{1,2,3}, {4,5,6},{7,8,9}};
         int [] result = MO.rowSums(arr);
+        for(int i : result)
+        {
+            System.out.println(i);
+        }
 
         int[][] arr1 = new int[3][3];
         for (int i = 0; i < arr.length; i++) 
