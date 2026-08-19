5fc55a5e30b004d0dd26451dcfe1559eaa5b4b19
Mohit <mohiitsiingh10@gmail.com>
2026-08-19 04:27:38 +0000

    Aug 19, 2026, 9:57 AM

--- Week-3/Week-3/src/MatrixOperations.java
+++ Week-3/Week-3/src/MatrixOperations.java
@@ -6,23 +6,23 @@ public class MatrixOperations {
         int[][] arr = new int[3][3];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
-
+                System.out.print("enter values for array row or column sum at " + i + " " + j + ": ");
                 arr[i][j] = input.nextInt();
 
             }
         }
-             int[][] arr1 = new int[3][3];
+        int[][] arr1 = new int[3][3];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
-
+  System.out.print("enter values for array 1 sum at " + i + " " + j + ": ");
                 arr1[i][j] = input.nextInt();
 
             }
         }
-             int[][] arr2 = new int[3][3];
+        int[][] arr2 = new int[3][3];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
-
+  System.out.print("enter values for array 2 sum at " + i + " " + j + ": ");
                 arr2[i][j] = input.nextInt();
 
             }
@@ -46,9 +46,10 @@ public class MatrixOperations {
 
         return rowsum;
     }
-    public int[] columnSum(int[][] matrix){
-    int[] ColumnSum = new int[matrix.length];
-     for (int i = 0; i < matrix[0].length; i++) {
+
+    public int[] columnSum(int[][] matrix) {
+        int[] ColumnSum = new int[matrix.length];
+        for (int i = 0; i < matrix[0].length; i++) {
             int sum = 0;
             for (int j = 0; j < matrix.length; j++) {
                 sum += matrix[j][i];
@@ -56,10 +57,11 @@ public class MatrixOperations {
             ColumnSum[i] = sum;
         }
         return ColumnSum;
-}
-public int[][] add(int[][] first, int[][] second){
-int [][] arr3 = new int[3][3];
- for (int i = 0; i < first.length; i++) {
+    }
+
+    public int[][] add(int[][] first, int[][] second) {
+        int[][] arr3 = new int[3][3];
+        for (int i = 0; i < first.length; i++) {
             for (int j = 0; j < first[0].length; j++) {
                 arr3[i][j] = first[i][j] + second[i][j];
             }
