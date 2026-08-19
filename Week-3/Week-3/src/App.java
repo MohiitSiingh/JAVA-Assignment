481536f696b4e37cc5134730dfc5cc4f27b65096
Mohit <mohiitsiingh10@gmail.com>
2026-08-19 04:07:36 +0000

    Aug 19, 2026, 9:37 AM

--- Week-3/Week-3/src/App.java
+++ Week-3/Week-3/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+          Scanner input = new Scanner(System.in);
+     MatrixOperations MO = new MatrixOperations();
+     MO.MatrixOperations_main(input);
+input.close();
     }
 }
--- Week-3/Week-3/src/MatrixOperations.java
+++ Week-3/Week-3/src/MatrixOperations.java
@@ -1,3 +1,33 @@
-public class MatrixOperations {
+import java.util.Scanner;
 
+public class MatrixOperations {
+    public void MatrixOperations_main(Scanner input) {
+        // Scanner input = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+
+                arr[i][j] = input.nextInt();
+
+            }
+        }
+        rowSums(arr);
+    }
+
+    public int[] rowSums(int[][] matrix) {
+
+        int[] rowsum = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < matrix[0].length; j++) {
+                sum += matrix[i][j];
+            }
+            rowsum[i] = sum;
+            // System.out.println("Sum of rows : " + sum);
+
+        }
+
+        return rowsum;
+    }
 }
+
