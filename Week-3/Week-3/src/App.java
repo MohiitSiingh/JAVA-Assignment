e7ef02262ec9a90ef54fd9f547e4f8d278481542
Mohit <mohiitsiingh10@gmail.com>
2026-08-19 04:49:55 +0000

    Aug 19, 2026, 10:19 AM

--- Week-3/Week-3/src/App.java
+++ Week-3/Week-3/src/App.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-          Scanner input = new Scanner(System.in);
-     MatrixOperations MO = new MatrixOperations();
-     MO.MatrixOperations_main(input);
+        Scanner input = new Scanner(System.in);
+        MatrixOperations MO = new MatrixOperations();
+        MO.MatrixOperations_main(input);
      
     
 input.close();
