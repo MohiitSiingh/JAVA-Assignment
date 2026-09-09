d77daeaf03da8f8bd7bdcfcad81aff45247f20ec
Mohit <mohiitsiingh10@gmail.com>
2026-09-09 04:23:03 +0000

    Sep 9, 2026, 9:53 AM

--- CampusManagementApp/src/App.java
+++ CampusManagementApp/src/App.java
@@ -5,6 +5,10 @@ public class App {
         Shapes s = new Circle("red", 5);
         s.CalcArea();
         double a = s.getArea();
-        
+        Shapes s1 = new Rectangle("Blue",4.5,5.5);
+        s1.CalcArea();
+        double rectangleArea = s1.getArea();
+        s1.CalcPerimeter();
+        double RectPeri = s1.getPerimeter();
     }
 }
--- CampusManagementApp/src/Rectangle.java
+++ CampusManagementApp/src/Rectangle.java
@@ -1,7 +1,59 @@
-public class Rectangle {
+public class Rectangle extends Shapes {
 
     private double length;
     private double width;
 
+    public double getLength()
+    {
+        return length;
+    }
+
+    public void SetLength(double l)
+    {
+        length = l ;
+    }
+
+    public double getWidth()
+    {
+        return width;
+    }
+
+    public void SetWidth(double w)
+    {
+        width = w;
+    }
+    public Rectangle(String c , double l, double w)
+    {
+        super(c);
+        if(l>=0)
+        {
+        length = l;
+        }
+        else 
+        {
+            length = 0;
+        }
+        if(w>=0)
+        {
+            width = w;
+        }
+        else
+        {
+            width = 0;
+        }
+    }
+
+    public void CalcArea()
+    {
+        area = length*width;
+    }
+    
+    public void CalcPerimeter()
+    {
+        perimeter = length*width;
+    }
+
+    
+        
     
 }
