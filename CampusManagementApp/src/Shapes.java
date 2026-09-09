7de4c65dd3bbf043a02753a573d557101f3dc796
Mohit <mohiitsiingh10@gmail.com>
2026-09-09 03:41:05 +0000

    Sep 9, 2026, 9:11 AM

---
+++ CampusManagementApp/src/Shapes.java
@@ -0,0 +1,13 @@
+public abstract class Shapes {
+    private String color;
+
+    public abstract void area();
+
+    public abstract void perimeter();
+
+Shapes (String color){
+this.color = color;
+
+}
+
+}
